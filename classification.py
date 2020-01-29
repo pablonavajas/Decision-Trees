3dd5f99c2d7f374c29016c6b70c92bef810c4337
Edward Pearce-Crump <ep1011@sprite05.doc.ic.ac.uk>
2020-01-29 16:32:17 +0000

    q2.1partial

--- classification.py
+++ classification.py
@@ -8,6 +8,7 @@
 ##############################################################################
 
 import numpy as np
+import math
 
 
 class DecisionTreeClassifier(object):
@@ -30,7 +31,7 @@ class DecisionTreeClassifier(object):
 
     def __init__(self):
         self.is_trained = False
-    
+        self.node = None
     
     def train(self, x, y):
         """ Constructs a decision tree classifier from data
@@ -53,14 +54,22 @@ class DecisionTreeClassifier(object):
         # Make sure that x and y have the same number of instances
         assert x.shape[0] == len(y), \
             "Training failed. x and y must have the same number of instances."
-        
-        
 
         #######################################################################
         #                 ** TASK 2.1: COMPLETE THIS METHOD **
         #######################################################################
-        
-        
+      
+        """ TEST
+        node = self.find_best_node(x,y) 
+        print(node.attribute)
+        print(node.split_point)
+        """
+    
+        #Trains the decision tree
+        self.node = self.induce_decision_tree(x,y)
+
+        #Saves the model to a file. """ CHECK """
+        np.save('decision_tree.npy', self.node) 
         
         # set a flag so that we know that the classifier has been trained
         self.is_trained = True
@@ -102,5 +111,184 @@ class DecisionTreeClassifier(object):
     
         # remember to change this if you rename the variable
         return predictions
+
+        #######################################################################
+        #                 ** HELPER FUNCTIONS **
+        #######################################################################
+
+    def induce_decision_tree(self, x, y):
+        """
+        Implementation of Algorithm 1 - Decision Tree Induction
+        Returns the root node of the decision tree.
+        """
+
+        """ CHECK: "Dataset cannot be split further """
+        #array of unique labels
+        unique = np.unique(y)
+        #find the best node (will test for info_gain next)
+        node = self.find_best_node(x, y)
+
+        #if all samples have the same label or there is no information to be 
+        #gained by splitting
+        if unique.size == 1 or node.info_gain == 0:
+            return Node(label = unique[0])
+        else:
+            #node = self.find_best_node(x, y)
+            children_datasets = self.split_dataset(x, y, node)
+
+            for child_dataset in children_datasets:
+                child_node = self.induce_decision_tree(child_dataset[0], child_dataset[1])
+                node.add_child(child_node)
+            
+            return node
         
+
+    def find_best_node(self, x, y):
+        """
+        Returns the best node based on x: an NxK NumPy array representing N training
+        instances of K attributes and y: an N-dim NumPy array containing the class label
+        for each N instance.
+        """
+
+        #initialise variables which store max info gain and
+        #the attribute and split point
+        max_information_gain = -1
+        attribute = -1
+        split_point = -1
+        
+        #get the size of the array x
+        row_size, column_size = x.shape
+        
+        #loop through each column of X
+        for col in range(column_size):
+
+            #sort attributes and labels based on column
+            x, y = self.sort(x, y, col)
+
+            #Find possible splitting points and, for each, their information gain
+            #(can start from 1 as we need to split)
+            for row in range(1,row_size):
+                #if x[row,col] is a splitting point
+                """ CHECK """
+                if x[row,col] != x[row-1,col]:
+                    #slice the labels into two child sets
+                    #child1 = [att_1, lab_1] = [x[:row], y[:row]]
+                    #child2 = [att_2, lab_2] = [x[row:], y[row:]]
+                    children = [child1, child2] = [y[:row], y[row:]]
+
+                    #calculate information gain
+                    info_gain = self.information_gain(y, children)
+
+                    #test if this is max information gain so far
+                    #if so, update holding values
+                    if info_gain > max_information_gain:
+                        max_information_gain = info_gain
+                        attribute = col
+                        split_point = x[row,col]
+
+        #create node with attribute and value of max info_gain
+        node = Node(attribute, split_point, max_information_gain)
+        
+        return node
+
+    def sort(self, x, y, column):
+        """
+        Returns the array of attributes and the array of labels based on the column
+        to be sorted.
+        """
+
+        x = x[x[:,column].argsort()]
+        y = y[x[:,column].argsort()]
+
+        return x, y
+
+    def information_gain(self, parent, children):
+        """
+        Calculates the information gain for a set of parent class labels, and a list
+        of child sets (each set contains labels).
+        """
+
+        #total number of samples in the parent
+        N = (parent.shape)[0] 
+       
+        #calculate the entropy of the parent
+        h_parent = self.entropy(parent)
+
+        #calculate the terms that when summed make up the second term in calculating IG
+        h_children = [((child.shape)[0])*(1/N)*self.entropy(child) for child in children]
+
+        h_bar_children = sum(h_children)
+
+        return h_parent - h_bar_children
+
+        
+    def entropy(self, dataset):
+        """
+        Calculates the entropy for a given dataset.
+        """
+
+        #obtain the counts of each unique label
+        unique, counts = np.unique(dataset, return_counts = True)
+       
+        #obtain an array of probabilities for each label
+        total = sum(counts)
+        probability = [i/total for i in counts]
+
+        #get an array of terms for calculating entropy
+        term = [i*math.log(i,2) for i in probability]
+
+        #apply the entropy formula
+        return -sum(term)
+
+
+    def split_dataset(self, x, y, node):
+        """
+        Splits the dataset into child sets based on the node condition.
+        """
+
+        #Sort the attribute matrix and label matrix based on node.attribute
+        x, y = self.sort(x, y, node.attribute)
+
+        sp = node.split_point
+        
+        #Slice the datasets according to node.split_point
+        child1 = [x[:sp], y[:sp]]
+        child2 = [x[sp:], y[sp:]]
+        children = [child1, child2]
+
+        return children
+
+
+class Node:
+    """
+    A Node object has the following data members:
+        - the attribute and split point that results in the most informative split for
+        some dataset (set to None if the Node is meant to contain only a label)
+        - the label (set to None by default if the Node is meant to hold an attribute 
+        and split_point)
+        - the two child nodes that are pointed to by the parent (binary tree)
+
+    It also has the add_child() method, which associates a child node with its parent
+    """
+
+    def __init__(self, attribute = None, split_point = None, info_gain = None, label = None):
+        #column
+        self.attribute = attribute
+        #row
+        self.split_point = split_point
+        self.info_gain = info_gain
+        self.label = label
+        self.child1 = None
+        self.child2 = None
+
+    def add_child(self, child_node):
+        if self.child1 == None:
+            self.child1 = child_node
+        elif self.child2 == None:
+            self.child2 = child_node
+        else:
+            print("Error adding child_node")
+
+
+
 
--- test_exercise1.py
+++ test_exercise1.py
@@ -6,9 +6,74 @@
 ##############################################################################
 
 import test_class_exercise1 as DS
+import numpy as np
+import math
+import classification as cl
 
 #data = DS.Dataset('data/simple1.txt')
 data = DS.Dataset('data/toy.txt')
 
 print(data.attributes)
 print(data.labels)
+
+sortedArr = data.attributes[data.attributes[:,1].argsort()]
+sortedLabels = data.labels[data.attributes[:,1].argsort()]
+
+row, col = sortedArr.shape
+
+print(sortedArr)
+print(sortedLabels)
+
+for column in range(1, col):
+    print(column)
+
+
+print((sortedArr.shape)[0])
+
+unique, counts = np.unique(sortedLabels, return_counts = True)
+print(np.unique(sortedLabels, return_counts = True))
+print(unique.size)
+print(unique[0])
+print(counts)
+total = sum(counts)
+#probability = [i/total for i in counts]
+#term = [i*math.log(i,2) for i in probability]
+#print(probability)
+#print(term)
+#print(-sum(term))
+#print(np.asarray((unique, counts)).T)
+
+node = cl.Node(label = 'C')
+print(node.label)
+print(node.attribute)
+
+
+"""
+point = 3
+
+#a_child1 = sortedArr[:point]
+l_child1 = sortedLabels[:point]
+
+#child1 = [a_1, l_1] = [sortedArr[:point], sortedLabels[:point]]
+
+#a_child2 = sortedArr[point:]
+l_child2 = sortedLabels[point:]
+
+#print(sortedArr)
+#print(sortedLabels)
+
+print(l_child1)
+print(l_child2)
+"""
+"""
+print(a_child2)
+print(l_child2)
+
+print(child1)
+print(child1[0])
+print(child1[1])
+print(a_1)
+print(l_1)
+
+print([data.attributes,data.labels])
+"""
