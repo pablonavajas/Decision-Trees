8d7a972bd205ff83ef801a23789434db6a6e8243
Iurie Tarlev <it119@ic.ac.uk>
2020-02-02 21:08:21 +0000

    added cross-validation class and the function inside it

--- eval.py
+++ eval.py
@@ -13,7 +13,8 @@
 ##############################################################################
 
 import numpy as np
-
+from classification import DecisionTreeClassifier
+import random
 
 class Evaluator(object):
     """ Class to perform evaluation
@@ -43,7 +44,7 @@ class Evaluator(object):
             Rows are ground truth per class, columns are predictions.
         """
 
-        if not class_labels:
+        if class_labels is None:
             class_labels = np.unique(annotation)
 
         confusion = np.zeros((len(class_labels), len(class_labels)), dtype=np.int)
@@ -209,3 +210,76 @@ class Evaluator(object):
         macro_f = np.average(f)
 
         return (f, macro_f)
+
+
+        #######################################################################
+        #                 ** TASK 3.6: COMPLETE THIS METHOD **
+        #######################################################################
+
+class CrossValidator(object):
+
+    # validate k_fold input from the user (private)
+    def _k_fold_assert(self, k_folds, rows):
+        assert (k_folds > 0 and k_folds <= rows), \
+            "Number of labels in the predicted class not equal to number of labels in the ground truth class"
+
+    # function to split the dataset (private)
+    def _split_dataset(self, dataset, k_folds, random_fold):
+
+        # get nr or rows of the dataset
+        rows = len(dataset)
+
+        # get length of one fold
+        validation_set_len = rows // k_folds
+
+        # get lower limit and upper limit for validation set rows
+        split_row_1 = (validation_set_len) * random_fold
+        split_row_2 = (validation_set_len) * (random_fold + 1)
+
+        # extract validation set from dataset
+        validation_set = dataset[split_row_1:split_row_2]
+
+        # extract training set from dataset
+        train_set = dataset[:split_row_1]
+        train_set = np.append(train_set, dataset[split_row_2:], axis=0)
+
+        return validation_set, train_set
+
+    # run cross-validation
+    def run(self, dataset, k_folds):
+        # validate k_fold input from the user
+        self._k_fold_assert(k_folds, len(dataset.attributes))
+
+        # initialise an array for accuracies
+        accuracy_array = np.empty(0);
+
+        # randomly split the data into k subsets and get validation performance
+        for fold in range(k_folds - 1):
+            random_fold = random.randint(0, k_folds - 1) # 0-based
+
+            validation_attributes, train_attributes = self._split_dataset(dataset.attributes, k_folds, random_fold)
+            validation_labels, train_labels = self._split_dataset(dataset.labels, k_folds, random_fold)
+
+            # training the decision tree
+            classifier = DecisionTreeClassifier()
+            classifier = classifier.train(train_attributes, train_labels)
+
+            # get predictions based on the training
+            predictions = classifier.predict(validation_attributes)
+
+            # evaluation initialiser
+            evaluator = Evaluator()
+            classes = np.unique(dataset.labels);
+
+            # build confusion matrix
+            confusion = evaluator.confusion_matrix(predictions, validation_labels, classes)
+
+            # get accuracy and append it to the array of accuracies
+            accuracy = evaluator.accuracy(confusion)
+            accuracy_array = np.append(accuracy, accuracy_array)
+
+        #return accuracies_array, an average and a standard dev of those accuracies
+        return accuracy_array, np.average(accuracy_array), np.std(accuracy_array)
+
+
+
