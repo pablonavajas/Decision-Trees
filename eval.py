dfcb2b26e932e97493dd86f5e8b2d9c1e7a17b5a
Iurie Tarlev <it119@ic.ac.uk>
2020-02-05 09:55:41 +0000

    q3.3 - 3.5 answered

--- eval.py
+++ eval.py
@@ -74,6 +74,7 @@ class Evaluator(object):
             confusion[idx_int][idx2_int] += 1
             i += 1
 
+
         return confusion
 
     def accuracy(self, confusion):
@@ -127,8 +128,11 @@ class Evaluator(object):
         #######################################################################
         n = len(confusion)
         for i in range(n):
-            p[i] = confusion[i][i] / np.sum(confusion[0:, [i]])
-
+            denominator = np.sum(confusion[0:, [i]])
+            if(denominator):
+                p[i] = confusion[i][i] / denominator
+            else:
+                p[i] = 0
         # You will also need to change this
         macro_p = np.average(p)
 
@@ -165,7 +169,11 @@ class Evaluator(object):
         # You will also need to change this
         n = len(confusion)
         for i in range(n):
-            r[i] = confusion[i][i] / np.sum(confusion[[i], 0:])
+            denominator = np.sum(confusion[[i], 0:])
+            if (denominator):
+                r[i] = confusion[i][i] / denominator
+            else:
+                r[i] = 0
 
         # You will also need to change this
         macro_r = np.average(r)
@@ -205,16 +213,28 @@ class Evaluator(object):
 
         n = len(confusion)
         for i in range(n):
-            f[i] = 2 * p[i] * r[i] / (p[i] + r[i])
+            denominator = p[i] + r[i]
+            if (denominator):
+                f[i] = 2 * p[i] * r[i] / denominator
+            else:
+                f[i] = 0
+
 
         macro_f = np.average(f)
 
         return (f, macro_f)
 
+    def print_four_eval_metrics(self, confusion):
+        print("Accuracy : %.5f" % self.accuracy(confusion))
+        print("Precision: %.5f" % self.precision(confusion)[1])
+        print("Recall   : %.5f" % self.recall(confusion)[1])
+        print("F1 Score : %.5f" % self.f1_score(confusion)[1])
 
-        #######################################################################
-        #                 ** TASK 3.6: COMPLETE THIS METHOD **
-        #######################################################################
+
+
+#######################################################################
+#                 ** TASK 3.6: COMPLETE THIS METHOD **
+#######################################################################
 
 class CrossValidator(object):
 
@@ -233,8 +253,8 @@ class CrossValidator(object):
         validation_set_len = rows // k_folds
 
         # get lower limit and upper limit for validation set rows
-        split_row_1 = (validation_set_len) * validation_fold
-        split_row_2 = (validation_set_len) * (validation_fold + 1)
+        split_row_1 = validation_set_len * validation_fold
+        split_row_2 = validation_set_len * (validation_fold + 1)
 
         # extract validation set from dataset
         validation_set = dataset[split_row_1:split_row_2]
@@ -245,13 +265,40 @@ class CrossValidator(object):
 
         return validation_set, train_set
 
+    def run_evaluation(self, validation_attributes, validation_labels, train_attributes, train_labels, unique_labels):
+
+        # training the decision tree
+        classifier = DecisionTreeClassifier()
+        classifier = classifier.train(train_attributes, train_labels)
+
+        # get predictions based on the training
+        predictions = classifier.predict(validation_attributes)
+
+        # evaluation initialiser
+        evaluator = Evaluator()
+        classes = np.unique(unique_labels);
+
+        # build confusion matrix
+        confusion = evaluator.confusion_matrix(predictions, validation_labels, classes)
+
+        # get accuracy and append it to the array of accuracies
+        accuracy = evaluator.accuracy(confusion)
+        p, macro_p = evaluator.precision(confusion)
+        r, macro_r = evaluator.recall(confusion)
+        f, macro_f = evaluator.f1_score(confusion)
+        eval_params = np.array([[accuracy, macro_p, macro_r, macro_f]], dtype=float)
+
+        return classifier, eval_params
+
     # run cross-validation
     def run(self, dataset, k_folds):
         # validate k_fold input from the user
         self._k_fold_assert(k_folds, len(dataset.attributes))
 
         # initialise an array for accuracies
-        accuracy_array = np.empty(0);
+        eval_data_frame = np.empty(0);
+        decision_trees = np.empty(0)#, type=DecisionTreeClassifier);
+
 
         # shuffle the dataset
         s = np.arange(dataset.labels.shape[0])
@@ -260,31 +307,66 @@ class CrossValidator(object):
         labels = dataset.labels[s]
 
         # randomly split the data into k subsets and get validation performance
-        for fold in range(k_folds - 1):
+        for fold in range(k_folds):
 
             validation_attributes, train_attributes = self._split_dataset(attributes, k_folds, fold)
             validation_labels, train_labels = self._split_dataset(labels, k_folds, fold)
 
-            # training the decision tree
-            classifier = DecisionTreeClassifier()
-            classifier = classifier.train(train_attributes, train_labels)
+            classifier, eval_params = self.run_evaluation(validation_attributes, validation_labels,
+                                                          train_attributes, train_labels, np.unique(labels))
 
-            # get predictions based on the training
-            predictions = classifier.predict(validation_attributes)
+            if fold == 0:
+                eval_data_frame = eval_params
+                decision_trees = classifier
+            else:
+                eval_data_frame = np.append(eval_data_frame, eval_params, axis=0)
+                decision_trees = np.append(decision_trees, classifier)
 
-            # evaluation initialiser
-            evaluator = Evaluator()
-            classes = np.unique(labels);
+        av = np.mean(eval_data_frame, axis=0)
+        std = np.std(eval_data_frame, axis=0)
+        max_val = np.max(eval_data_frame, axis=0)
 
-            # build confusion matrix
-            confusion = evaluator.confusion_matrix(predictions, validation_labels, classes)
+        eval_data_frame = np.append(eval_data_frame, av)
+        eval_data_frame = np.append(eval_data_frame, std)
+        eval_data_frame = np.append(eval_data_frame, max_val)
 
-            # get accuracy and append it to the array of accuracies
-            accuracy = evaluator.accuracy(confusion)
-            accuracy_array = np.append(accuracy, accuracy_array)
+        return decision_trees, eval_data_frame
 
-        #return accuracies_array, an average and a standard dev of those accuracies
-        return accuracy_array, np.average(accuracy_array), np.std(accuracy_array)
+    def print_evaluation_params(self, eval_data_frame):
+        eval_data_frame = eval_data_frame.reshape(-1, 4)
+
+        print("                   Accuracy    Precision    Recall    F1 ")
+        nr_of_rows = eval_data_frame.shape[0]
+        for row in range(nr_of_rows):
+            if row < nr_of_rows - 3:
+                row_label = "Decision Tree " + str(row)
+            elif row < nr_of_rows - 2:
+                row_label = "Average        "
+            elif row < nr_of_rows - 1:
+                row_label = "St. Dev.       "
+            else:
+                row_label = "Max Value      "
+
+            print(" %s   %.5f     %.5f      %.5f   %.5f" % (row_label, eval_data_frame[row][0],
+                     eval_data_frame[row][1], eval_data_frame[row][2], eval_data_frame[row][3]))
+
+    #get an array of mode for all columns of array (n x k)
+    def mode_2d(self, array_n_x_k):
+        k = array_n_x_k.shape[1]
+        mode_array = np.empty(0)
+        for i in range(k):
+            value, count = np.unique(array_n_x_k[:, i], return_counts=True)
+            indices = (-count).argsort() #minus for descending order
+            value = value[indices]       #rearrange in descending order of count
+            mode_array = np.append(mode_array, value[0])
+        return mode_array
 
 
+    def get_tree_with_max_accuracy(self, classifiers_and_eval_params):
+        classifiers, eval_data_frame = classifiers_and_eval_params
+        eval_data_frame = eval_data_frame.reshape(-1, 4)
 
+        nr_trees = len(eval_data_frame) - 3
+        accuracy_array = eval_data_frame[:nr_trees, 0]
+        max_accuracy_index = np.argmax(accuracy_array)
+        return classifiers[max_accuracy_index]
--- train_full.py
+++ train_full.py
@@ -29,6 +29,67 @@ if __name__ == "__main__":
     #######################################################################
     print("Performing cross-validation...")
     cv = CrossValidator()
-    accuracy = cv.run(dataset, 10)
-    print(accuracy)
+    cross_validation_output = cv.run(dataset, 5)
+    cv.print_evaluation_params(cross_validation_output[1])
+
+    #######################################################################
+    # ** QUESTION 3.4: EVALUATE MODEL TRAINED ON FULL DATASET  **
+    # ** BEST ACCURACY MODEL FROM A SUBSET (CROSS-VALIDATED)   **
+    #######################################################################
+    print("\n\nQUESTION 3.4: BEST PERFORMING CLASSIFIER FROM CROSS-VAL vs CLASSIFIER TRAINED ON FULL DATASET")
+    # get test labels
+    test_dataset = Dataset("data/test.txt")
+
+    # EVALUATION OF CLASSIFIER TRAINED ON FULL DATASET
+    predictions = classifier.predict(test_dataset.attributes)
+
+    # evaluation initializer
+    evaluator = Evaluator()
+
+    # build confusion matrix
+    confusion = evaluator.confusion_matrix(predictions, test_dataset.labels)
+
+    # get accuracy and append it to the array of accuracies
+    print("\nEvaluation Parameters (using test.txt) of tree trained on full dataset ...")
+    evaluator.print_four_eval_metrics(confusion)
+
+
+    # EVALUATION ON THE BEST ACCURACY MODEL (FROM CROSS-VALIDATION)
+    # choose the best classifier
+    best_acc_cross_valid_classifier = cv.get_tree_with_max_accuracy(cross_validation_output)
+
+    predictions = best_acc_cross_valid_classifier.predict(test_dataset.attributes)
+
+    # evaluation initializer
+    evaluator = Evaluator()
+
+    # build confusion matrix
+    confusion = evaluator.confusion_matrix(predictions, test_dataset.labels)
+
+    # get accuracy and append it to the array of accuracies
+    print("\nEvaluation Parameters on of the best performing tree on test.txt ...")
+    evaluator.print_four_eval_metrics(confusion)
+
+    #######################################################################
+    #         ** QUESTION 3.5: COMBINE PREDICTIONS OF 10 TREES **
+    #######################################################################
+    print("\n\nQUESTION 3.5: COMBINED PREDICTIONS OF CLASSIFIERS TRAINED ON SUBSET OF DATA")
+
+    # combine predictions from all 10 trees into one array called predictions
+    predictions_combined = np.asarray(cross_validation_output[0][0].predict(test_dataset.attributes))
+    for i in range(1, 5):
+        predictions_combined = np.vstack((predictions, np.asarray(cross_validation_output[0][i].predict(test_dataset.attributes))))
+
+    # get mode of those predictions across 10 trees
+    predictions_combined_mode = cv.mode_2d(predictions_combined)
+
+    print("\nEvaluation Parameters using combined predictions from 10 decision trees ...")
+    evaluator = Evaluator()
+    confusion = evaluator.confusion_matrix(predictions_combined_mode, test_dataset.labels)
+    evaluator.print_four_eval_metrics(confusion)
+
+
+
+
+
 
